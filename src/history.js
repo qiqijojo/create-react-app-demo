e380be89fdf3ff1791f55041fdee5c88d1dbe6e4
王齐 qi.wang01 <qi.wang01@weimob.com>
2018-05-24 13:44:43 +0800

    history

---
+++ src/history.js
@@ -0,0 +1,3 @@
+import createHistory from 'history/es/createBrowserHistory';
+
+export default createHistory();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import {Router, Route, IndexRoute} from 'react-router';
+import thunk from 'redux-thunk';
+import history from './history';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(applyMiddleware(thunk));
+ReactDOM.render(
+    // <App />, 
+    <Provider store={store}>
+        {/* <App/> */}
+    <Router history={history}>
+      <Route path="/" component={App}>
+        {/* <Route path="foo" component={Foo}/>
+        <Route path="bar" component={Bar}/> */}
+      </Route>
+    </Router>
+  </Provider>,
+    document.getElementById('root')
+);
 // registerServiceWorker();
